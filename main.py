38cd407803ee6969d6d8ef6056583d248ba66abf
ArmaanThadani <armaanthadani2007@gmail.com>
2024-07-26 14:21:32 -0400

    Hello

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                running = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 
 
