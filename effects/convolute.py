10c1735905b77019ed0c43943ff8ae93767a21f0
Om Mehta <ommehta16@icloud.com>
2024-07-28 21:29:31 -0400

    Implement convolution, gaussian blur and box blur in convolute.

--- effects/convolute.py
+++ effects/convolute.py
@@ -3,23 +3,52 @@ import numpy as np
 import math
 import sys
 
-def convolute(img:Image,conv_matrix:list[list[int]]) -> Image:
+
+
+def convolute(img,conv:np.array) -> Image:
     
-    # Apply the convolution matrix to the image
-    # Try making this vectorized so that it's fast
+    img_arr = np.array(img).astype(int)
     
-    return img
+    conv_x = conv.shape[1]
+    conv_y = conv.shape[0]
+    
+    new_img = np.zeros(img_arr.shape).astype(int)
+    
+    for c in range(3):
+        curr_channel = np.pad(img_arr[:,:,c],(conv_y//2,conv_x//2),'constant',constant_values=(0,0))
+        
+        for y in range(new_img.shape[0]):
+            for x in range(new_img.shape[1]):
+                curr_block = curr_channel[y:y+conv_y,x:x+conv_x]
+                new_img[y,x,c] = np.multiply(curr_block,conv).sum()
+    
+    new_img = new_img.clip(0,255)
+    return Image.fromarray(new_img.astype(np.uint8))
 
 class Blur:
-    def gaussian(img:Image,radius:int|float) -> Image:
-        
-        # Apply the gaussian blur using `convolute(img,matrix)`
+    
+    def gaussian(img:Image,radius:int|float,sigma:float) -> Image:
+        def generate_kernel(rad,sigma) -> np.array:
+            arr = np.zeros((2*rad+1,2*rad+1))
+            inv_sigma = 1/sigma
+            d_squared = lambda x,y: x**2 + y**2
+            G = lambda x,y: 0.159154943 * inv_sigma * math.exp(-0.5 * inv_sigma * d_squared(x-rad,y-rad))
+            for y in range(2*rad+1):
+                for x in range(2*rad+1):
+                    arr[x,y] = G(x,y)
+                    
+            return arr/arr.sum()
+        img = convolute(img,generate_kernel(radius,sigma))
         
         return img
     
     def box(img:Image, size:int|float) -> Image:
+        def generate_kernel(rad) -> np.array:
+            arr = np.array(1,(size*2+1,size*2+1))
+            arr = arr * 1/(size*2+1)**2
+            return arr
         
-        # Apply the box blur using `convolute(img,matrix)`
+        img = convolute(img,generate_kernel(size))
         
         return img
     
